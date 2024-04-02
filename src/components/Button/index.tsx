f0f1803090f7e9ee6d1353b569b3dd8ef60980b0
Nick Ramsey <nramseysc@gmail.com>
2024-04-02 00:19:15 -0600

    first story

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,4 +1,12 @@
-export const Button = () => {
-  return <button className="text-blue-500">Button</button>;
+import { ComponentProps } from "react";
+
+type ButtonProps = ComponentProps<"button">
+
+export const Button = ({...props}: ButtonProps) => {
+  return ( 
+    <button className="text-blue-500" {...props}>
+      Button
+    </button>
+  );
 };
 

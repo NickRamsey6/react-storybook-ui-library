21bd5b987d2ebbbc83006e98ff7b5d07d2482573
Nick Ramsey <nramseysc@gmail.com>
2024-04-04 15:20:00 -0600

    setup stack layout component

--- src/components/Layout/Box/index.tsx
+++ src/components/Layout/Box/index.tsx
@@ -1,6 +1,6 @@
-import { ComponentProps, forwardRef } from "react";
+import { ComponentPropsWithRef, forwardRef } from "react";
 
-type BoxProps = ComponentProps<"div">;
+export type BoxProps = ComponentPropsWithRef<"div">;
 
 export const Box = forwardRef<HTMLDivElement, BoxProps>(({ ...props }, ref) => {
     return <div ref={ref} {...props} />;

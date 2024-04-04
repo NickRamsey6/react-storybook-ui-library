224339c5b4f65d008665de6f35e23d433d18fdaf
Nick Ramsey <nramseysc@gmail.com>
2024-04-03 19:30:08 -0600

    add solid, outline and ghost button variants

--- src/components/Button/index.stories.tsx
+++ src/components/Button/index.stories.tsx
@@ -14,11 +14,23 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
+export const Solid: Story = {
     args: {
-        children: 'Click me',
         variant: "solid",
-        size: "md",
-        colorScheme: "primary",
+        children: "Button",
+    },
+};
+
+export const Outline: Story = {
+    args: {
+        variant: "outline",
+        children: "Button",
+    },
+};
+
+export const Ghost: Story = {
+    args: {
+        variant: "ghost",
+        children: "Button",
     },
 };
